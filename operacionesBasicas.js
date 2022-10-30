451f1c992865a59d51870d0e2fe0d5f282c9e102
Ossmanp <ossman.puerto@cedinsi.edu.co>
2022-10-29 20:57:30 -0500

    no se hizo nada raro

---
+++ operacionesBasicas.js
@@ -0,0 +1,5 @@
+/** Realizar el algoritmo que realice las 4 operaciones básicas para dos números
+de entrada.*/
+'use strict';
+let numero1 = prompt('Digite el primer número',0);
+let numero2 = prompt('Digite el segundo número',0);
