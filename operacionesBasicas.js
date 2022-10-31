b75feb0eaf9224d6f9fc9c8951056a1954db4af7
Ossmanp <ossman.puerto@cedinsi.edu.co>
2022-10-30 20:30:00 -0500

    realización del ejercicio dos

--- operacionesBasicas.js
+++ operacionesBasicas.js
@@ -1,5 +1,27 @@
 /** Realizar el algoritmo que realice las 4 operaciones básicas para dos números
 de entrada.*/
 'use strict';
-let numero1 = prompt('Digite el primer número',0);
-let numero2 = prompt('Digite el segundo número',0);
+
+let numero1 = 2;
+let numero2 = 3;
+let operacion ="multiplicación";
+let resultado = "";
+
+switch(operacion){
+    case 'suma':
+        resultado = numero1 + numero2;
+        break;
+    case 'resta':
+        resultado = numero1 - numero2;
+        break;
+    case 'multiplicación':
+        resultado = numero1 * numero2;
+        break;
+    case 'división':
+        resultado = numero1 / numero2;
+        break;
+    default:
+        resultado = 'la operación no es válida';
+        break;
+}
+console.log(resultado)
